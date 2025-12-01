eb07bb9b4e1ae6d1369fa0cb64ee4b2ae9d7927e
CodingBill <91626615+Bilboramix@users.noreply.github.com>
2025-12-01 01:20:50 +0100

    POC done

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,64 +1,143 @@
 "use client";
 
-import { Config as PuckConfig, Puck } from "@measured/puck";
+import { ComponentConfig, createUsePuck, Puck, Config as PuckConfig, Data as PuckData, UiState as PuckUiState, AppState, Fields } from "@measured/puck";
 import "@measured/puck/puck.css";
 
-// Create Puck component config
-const config: PuckConfig = {
+// Augment the type of puck to transit the state we want through the editor
+type TAugmentedPuckUiState = PuckUiState & { advancedMode: boolean };
+type TAugmentedPuckAppState = Pick<AppState, "data"> & { ui: TAugmentedPuckUiState };
+
+// Components configs
+
+type TTextBlockProps = {
+  text: string;
+  advancedText?: string;
+};
+
+const textBlockConfig: ComponentConfig<TTextBlockProps> = {
+  resolveFields: (_, params) => {
+    const uiState = params.appState.ui as TAugmentedPuckUiState;
+    const fields: Fields<TTextBlockProps> = { text: { type: "text" } };
+
+    console.log("resolveFields - textBlockConfig - advancedMode :", uiState.advancedMode);
+    if (uiState.advancedMode) fields.advancedText = { type: "text" };
+
+    return fields;
+  },
+
+  render: ({ text: someText }) => <h1>{someText}</h1>,
+};
+
+type TNumberBlockProps = {
+  number: number;
+  advancedNumber?: number;
+};
+
+const numberBlockConfig: ComponentConfig<TNumberBlockProps> = {
+  resolveFields: (_, params) => {
+    const uiState = params.appState.ui as TAugmentedPuckUiState;
+    const fields: Fields<TNumberBlockProps> = { number: { type: "number" } };
+
+    console.log("resolveFields - numberBlockConfig - advancedMode :", uiState.advancedMode);
+    if (uiState.advancedMode) fields.advancedNumber = { type: "number" };
+
+    return fields;
+  },
+  render: ({ number: theNumber }) => <p>The number is {theNumber}</p>,
+};
+
+// Global config
+
+type TComponentsProps = {
+  TextBlock: TTextBlockProps;
+  NumberBlock: TNumberBlockProps;
+};
+
+const config = {
   components: {
-    TestBlock: {
-      fields: {
-        test: {
-          type: "custom",
-          render: ({ value, onChange }) => {
-            const onClickTest = () => onChange(new Date());
-            console.log("rendering field with value:", value);
-
-            return (
-              <div style={{ color: "black" }}>
-                <button onClick={onClickTest}>Click !</button>
-                {!value && <p>No value</p>}
-                {value && <div>Test value : {JSON.stringify(value)}</div>}
-              </div>
-            );
-          },
-        },
-        testNested: {
-          type: "custom",
-          render: ({ value, onChange }) => {
-            const onClickTest = () => onChange({ myDate: new Date(), deep: { myDeepDate: new Date() } });
-            console.log("rendering field with value:", value);
-
-            return (
-              <div style={{ color: "black" }}>
-                <button onClick={onClickTest}>Click !</button>
-                {!value && <p>No value</p>}
-                {value && (
-                  <div>
-                    Test value : <pre>{JSON.stringify(value, null, 2)}</pre>
-                  </div>
-                )}
-              </div>
-            );
-          },
-        },
-      },
-      render: ({ children }) => {
-        return <h1>{children}</h1>;
-      },
+    TextBlock: textBlockConfig,
+    NumberBlock: numberBlockConfig,
+  },
+  root: {
+    resolveFields: (data, params) => {
+      const uiState = params.appState.ui as TAugmentedPuckUiState;
+      console.log("resolveFields - rootConfig - advancedMode :", uiState.advancedMode);
+      return {};
     },
   },
+} as const satisfies PuckConfig<TComponentsProps>;
+type TPuckConfig = typeof config;
+
+// Initial data
+
+type TData = PuckData<TComponentsProps>;
+const initialData: TData = {
+  root: { props: {} },
+  zones: {},
+  content: [
+    {
+      type: "TextBlock",
+      props: {
+        id: "id",
+        text: "❤️",
+      },
+    },
+  ],
 };
 
-// Describe the initial data
-const initialData = {};
+const logData = (data: TData) => console.log(JSON.stringify(data, null, 2));
+const usePuck = createUsePuck<TPuckConfig>();
 
-// Save the data to your database
-const save = (data: any) => {
-  console.log("data", data);
-};
-
-// Render Puck editor
 export function PuckEditor() {
-  return <Puck config={config} data={initialData} onPublish={save} />;
+  return (
+    <Puck
+      config={config}
+      data={initialData}
+      onPublish={logData}
+      overrides={{
+        fields: ({ children }) => {
+          const dispatch = usePuck(({ dispatch }) => dispatch);
+          const puckState = usePuck(({ appState }) => appState) as TAugmentedPuckAppState;
+          const selectedItem = usePuck(({ selectedItem }) => selectedItem);
+          const getSelectorForId = usePuck(({ getSelectorForId }) => getSelectorForId);
+
+          const advancedMode = puckState.ui.advancedMode ?? false;
+
+          const nastyHack = () => {
+            // Store the value of the state
+            dispatch({
+              type: "setUi",
+              ui: { ...puckState.ui, advancedMode: !advancedMode } as TAugmentedPuckUiState,
+            });
+
+            if (selectedItem) {
+              const selector = getSelectorForId(selectedItem.props.id);
+              if (!selector) throw new Error("Selector not found");
+              // Triggers resolvers of the selected component
+              dispatch({
+                type: "replace",
+                destinationIndex: selector.index,
+                destinationZone: selector.zone,
+                data: { ...selectedItem },
+              });
+            } else {
+              // Triggers resolvers of the root config
+              dispatch({
+                type: "replaceRoot",
+                root: { ...puckState.data.root },
+              });
+            }
+          };
+
+          return (
+            <>
+              <button onClick={() => nastyHack()}>Set advanced mode to : {(!advancedMode).toString()}</button>
+              <p>advancedMode : {advancedMode.toString()}</p>
+              {children}
+            </>
+          );
+        },
+      }}
+    />
+  );
 }
